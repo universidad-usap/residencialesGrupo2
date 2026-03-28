edbbfe274aa1f28f3e90fc9274f558c206fea125
JoseSevilla2002 <1200700@usap.edu>
2026-03-28 13:52:03 -0600

    Update app.js

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express= require('express');
+const morgan = require('morgan');
+const path= require('path');
+const mysql= require('mysql');
+const cors = require('cors');
+const myConnection= require('express-myconnection');
+const app= express();
+
+const config = {
+    application: {
+        cors: {
+            server: [
+                {
+                    origin: "localhost:3000", 
+                    credentials: true
+                }
+            ]
+        }
+}
+};
+
+app.use(cors(
+    config.application.cors.server
+  ));
+
+const bancosRoutes = require('./rutas/bancos');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use(myConnection(mysql,{
+    host:'localhost',
+    user:'root',
+    password:'Sierra84$',
+    port:3306,
+    database:'academics'
+}, 'single'));
+app.use(express.urlencoded({extended: false}));
+
+var bodyParser = require('body-parser');
+
